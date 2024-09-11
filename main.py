6987962019913c145a615d02a3e6d570d3610a3f
Yan Dias <yandias123.yd@gmail.com>
2024-09-11 14:56:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,21 @@ def main():
         elif escolha == 5:
             pass
         elif escolha == 6:
-            pass
+            print('''
+1.  O Senhor dos Anéis: A sociedade do anel
+2.  O Hobbit: Uma viagem inesperada
+3.  Harry pobre e a pedra de Crack
+4.  As Crônicas de Nárnia: O leão, a Feiticeira e o Guarda-roupa
+5.  Alice no País das Maravilhas
+6.  A Bússula de Ouro
+7.  O Labirinto do Fauno
+8.  Coração de Dragão
+9.  Percy Jackson e o ladrão de raios
+10. Eragon
+
+
+
+''')
         elif escolha == 7:
             pass
         elif escolha == 8:
